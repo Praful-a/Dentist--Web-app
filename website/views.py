fc50b66d58ba1cdf9d51af52bbce7799808aeb68
Praful-a <prafuldubey27@gmail.com>
2020-09-04 22:21:15 +0530

    my first commit.

---
+++ website/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+# Create your views here.
+
+
+def view(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        # send an Email
+        send_mail(
+            name,  # subject
+            message,  # Message
+            email,  # from email
+            ['rock68232@gmail.com'],  # To Email
+
+        )
+
+        return render(request, 'contact.html', {'name': name})
+    else:
+        return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
+
+
+def service(request):
+    return render(request, 'service.html', {})
+
+
+def appointment(request):
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_scheldule = request.POST['your-scheldule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+
+        # send an Email
+        appointment = "Name: {} Phone: {} Email: {} Address: {} Scheldule: {} Day: {} Message: {}".format(
+            your_name, your_phone, your_email, your_address, your_scheldule, your_date, your_message)
+        send_mail('Appointment Request',  # subject
+                  appointment,  # Message
+                  your_email,  # from email
+                  ['rock68232@gmail.com'],  # To Email
+                  )
+
+        return render(request, 'appointment.html', {
+            'your_name': your_name,
+            'your_phone': your_phone,
+            'your_email': your_email,
+            'your_address': your_address,
+            'your_scheldule': your_scheldule,
+            'your_date': your_date,
+            'your_message': your_message})
+    else:
+        return render(request, 'home.html', {})
